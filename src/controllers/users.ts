47b2ab0d264e5b47f5ca9fb8920db60dbb5398d7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-08-06 23:00:26 +1000

    scores and great count

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -52,9 +52,13 @@ const showUser = async (user: User, orm: MikroORM<PostgreSqlDriver>) => {
     };
   }, {});
 
+  let scoreCount = 0
+
   const summary: ScoreSummary[] = songs.map((song) => {
     const score = songIdAsKey[song.id];
     if (score) {
+      scoreCount += 1
+
       return {
         id: score.id.toString(),
         marvelous: (score.marvelous || 0).toString(),
@@ -87,7 +91,10 @@ const showUser = async (user: User, orm: MikroORM<PostgreSqlDriver>) => {
     };
   });
 
-  return summary.sort((x, y) => x.song.name.localeCompare(y.song.name));
+  return {
+    scores: summary.sort((x, y) => x.song.name.localeCompare(y.song.name)),
+    scoreCount
+  }
 };
 
 users.get("/", async (req: Request, res: Response) => {
@@ -99,7 +106,14 @@ users.get("/:username", async (req: Request, res: Response) => {
   const user = await orm.em.findOne(User, { username: req.params.username });
   const currentUser = req.user as User
 
-  let scores = user ? await showUser(user!, orm as MikroORM<PostgreSqlDriver>) : []
+  let { scores, scoreCount } = user
+    ? await showUser(user!, orm as MikroORM<PostgreSqlDriver>)
+    : { scores: [], scoreCount: 0 };
+
+  const greats = scores.reduce((acc, curr) => {
+    const asInt = parseInt(curr.great, 10)
+    return acc + (isNaN(asInt) ? 0 : asInt)
+  }, 0)
 
   const foundLetter = new Set<string>()
   scores = scores.map<ScoreSummary & { anchor?: string }>((score) => {
@@ -120,8 +134,10 @@ users.get("/:username", async (req: Request, res: Response) => {
     user,
     letters: [...foundLetter].sort((x, y) => x.localeCompare(y)),
     name: req.params.username,
+    scoreCount: `${scoreCount} / ${scores.length}`,
     flag: user && user.region && countryMap[user.region]?.flag,
     canEdit: currentUser && user && currentUser.id === user.id,
-    scores
+    scores,
+    greats
   });
 });
